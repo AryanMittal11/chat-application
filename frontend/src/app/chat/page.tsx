26cec227b006a5af67f531d17b11474a6ee65280
AryanMittal11 <mittalaryan1110@gmail.com>
2026-02-19 00:09:01 +0530

    chat-sidebar

--- frontend/src/app/chat/page.tsx
+++ frontend/src/app/chat/page.tsx
@@ -1,11 +1,36 @@
 "use client"
+import ChatSidebar from '@/components/ChatSidebar'
 import Loading from '@/components/Loading'
-import { useAppData } from '@/context/AppContext'
+import { useAppData, User } from '@/context/AppContext'
 import { useRouter } from 'next/navigation'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+
+export interface Message {
+  _id: string;
+  chatId: string;
+  sender: string;
+  text?: string;
+  image?: {
+    url: string;
+    publicId: string;
+  };
+  messageType: "text" | "image";
+  seen: boolean;
+  seenAt: string;
+  createdAt: string;
+}
 
 const ChatApp = () => {
-  const {loading, isAuth} = useAppData()
+  const {loading, isAuth, logoutUser, chats, user: loggedInUser, users, fetchChats, setChats} = useAppData()
+
+  const [selectedUser, setSelectedUser] = useState<string | null>(null)
+  const [message, setMessage] = useState("")
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+  const [messages, setMessages] = useState<Message[] | null>(null)
+  const [user, setUser] = useState<User | null>(null)
+  const [showAllUser, setShowAllUser] = useState(false)
+  const [isTyping, setIsTyping] = useState(false)
+  const [typingTimeOut, setTypingTimeOut] = useState<NodeJS.Timeout | null>(null)
 
   const router = useRouter();
 
@@ -15,9 +40,13 @@ const ChatApp = () => {
     }
   }, [isAuth, router, loading])
 
+  const handleLogout = () => logoutUser();
+
   if (loading) return <Loading />
   return (
-    <div>ChatApp</div>
+    <div className='min-h-screen flex bg-gray-900 text-white relative overflow-hidden'>
+      <ChatSidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} showAllUser={showAllUser} setShowAllUser={setShowAllUser} users={users} loggedInUser={loggedInUser} chats={chats} selectedUser={selectedUser} setSelectedUser={setSelectedUser} handleLogout={handleLogout}/>
+    </div>
   )
 }
 
